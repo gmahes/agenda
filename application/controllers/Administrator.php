6e676b523f01f06691ecf5d0d3925a5f5262e054
gmahes <gregi.mahes@gmail.com>
2023-10-04 15:43:55 +0700

    add employees page administrator & login filter

--- application/controllers/Administrator.php
+++ application/controllers/Administrator.php
@@ -3,6 +3,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Administrator extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->session->userdata('username')) {
+            $this->session->set_flashdata('message', '<div class="alert alert-warning" role="alert">
+                Anda belum login!</div>');
+            redirect('auth/login');
+        } elseif ($this->session->userdata('username')) {
+            if (!$this->session->userdata('role') == 1) {
+                echo "anda tidak bisa mengakses halaman ini";
+            }
+        }
+    }
     public function index()
     {
         $data = [
@@ -12,6 +25,17 @@ class Administrator extends CI_Controller
         $this->load->view('administrators/templates/topbar');
         $this->load->view('administrators/templates/sidebar');
         $this->load->view('administrators/index');
+        $this->load->view('administrators/templates/footer');
+    }
+    public function employees()
+    {
+        $data = [
+            'title' => 'Dashboard'
+        ];
+        $this->load->view('administrators/templates/header', $data);
+        $this->load->view('administrators/templates/topbar');
+        $this->load->view('administrators/templates/sidebar');
+        $this->load->view('administrators/employees');
         $this->load->view('administrators/templates/footer');
     }
 }
---
+++ application/views/administrators/employees.php
@@ -0,0 +1,10 @@
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <!-- start content -->
+            <h1 class="mt-4">Master Data Karyawan</h1>
+            <!-- end content -->
+        </div>
+    </main>
+</div>
+</div>
