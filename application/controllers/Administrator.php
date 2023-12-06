a5735939e47aa5bed0bda60c7e8bb1e6b6406d5d
gmahes <gregi.mahes@gmail.com>
2023-12-06 14:06:20 +0700

    fix admin model

--- application/controllers/Administrator.php
+++ application/controllers/Administrator.php
@@ -17,14 +17,7 @@ class Administrator extends CI_Controller
     }
     public function index()
     {
-        $data = [
-            'title' => 'Dashboard',
-            'user' => $this->db->get('user_details'),
-            'agenda' => $this->db->get_where('agenda_details', ['is_verified' => 'accepted'])->result_array(),
-            'agenda_count' => $this->db->get_where('agenda_details', ['is_verified' => 'not_verified'])->num_rows(),
-            'agenda_approve' => $this->db->get_where('agenda_details', ['is_verified' => 'accepted'])->num_rows(),
-            'agenda_declined' => $this->db->get_where('agenda_details', ['is_verified' => 'declined'])->num_rows(),
-        ];
+        $data = data_index();
         $this->load->view('administrators/templates/header', $data);
         $this->load->view('administrators/templates/topbar');
         $this->load->view('administrators/templates/sidebar');
@@ -33,11 +26,7 @@ class Administrator extends CI_Controller
     }
     public function employees()
     {
-        $all_user = $this->db->get_where('user_details', ['id !=' => 1])->result_array();
-        $data = [
-            'title' => 'Dashboard',
-            'user' => $all_user
-        ];
+        $data = data_employees();
         $this->load->view('administrators/templates/header', $data);
         $this->load->view('administrators/templates/topbar');
         $this->load->view('administrators/templates/sidebar');
@@ -46,50 +35,18 @@ class Administrator extends CI_Controller
     }
     public function create()
     {
-        $this->form_validation->set_rules('username', 'Username', 'trim|required|is_unique[user_details.username]|alpha_numeric');
-        $this->form_validation->set_rules('password', 'Password', 'required');
-        $this->form_validation->set_rules('first_name', 'First Name', 'trim|required');
-        $this->form_validation->set_rules('last_name', 'Last Name', 'trim');
-        $this->form_validation->set_rules('role', 'Role', 'required');
-        if ($this->form_validation->run() == false && $_POST) {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger mt-2" role="alert">' . validation_errors() . '</div>');
-            redirect('administrator/employees');
-        }
+        create_validation();
         if ($_POST) {
-            $data = [
-                'username'   => $this->input->post('username'),
-                'password'   => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
-                'first_name' => $this->input->post('first_name') . ' ',
-                'last_name'  => $this->input->post('last_name'),
-                'role'       => $this->input->post('role')
-            ];
-            $this->db->insert('user_details', $data);
+            $this->Administrator_model->create_user();
             $this->session->set_flashdata('message', '<div class="alert alert-success mt-2" role="alert">Tambah karyawan baru berhasil!</div>');
         }
         redirect('administrator/employees');
     }
     public function edit()
     {
-        $this->form_validation->set_rules('first_name', 'First Name', 'trim|required');
-        $this->form_validation->set_rules('last_name', 'Last Name', 'trim');
-        $this->form_validation->set_rules('role', 'Role', 'required');
-        if ($this->form_validation->run() == false and $_POST) {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger mt-2" role="alert">' . validation_errors() . '</div>');
-            redirect('administrator/employees');
-        }
+        edit_validation();
         if ($_POST) {
-            $data = [
-                'id'         => $this->input->post('id'),
-                'username'   => $this->input->post('username'),
-                'first_name' => $this->input->post('first_name') . ' ',
-                'last_name'  => $this->input->post('last_name'),
-                'role'       => $this->input->post('role'),
-            ];
-            $data_agenda = [
-                'agenda_taskperson' => $this->input->post('first_name') . $this->input->post('last_name')
-            ];
-            $this->db->where('id', $this->input->post('id'))->update('user_details', $data);
-            $this->db->where('user_id', $this->input->post('id'))->update('agenda_details', $data_agenda);
+            $this->Administrator_model->edit_user();
             $this->session->set_flashdata('message', '<div class="alert alert-success mt-2" role="alert">Edit karyawan berhasil!</div>');
         }
         redirect('administrator/employees');
@@ -97,20 +54,14 @@ class Administrator extends CI_Controller
     public function delete()
     {
         if ($_POST) {
-            $id = $this->input->post('id');
-            $this->db->delete('user_details', ['id' => $id]);
+            $this->Administrator_model->delete_user();
             $this->session->set_flashdata('message', '<div class="alert alert-success mt-2" role="alert">Hapus karyawan berhasil!</div>');
         }
         redirect('administrator/employees');
     }
     public function agenda()
     {
-        $all_agenda = $this->db->get_where('agenda_details', ['is_verified' => 'not_verified'])->result_array();
-        $data = [
-            'title' => 'Dashboard',
-            'agenda' => $all_agenda,
-            'taskperson' => $this->db->select('first_name, last_name')->from('user_details')->join('agenda_details', 'user_details.id = agenda_details.agenda_taskperson')->get()->result_array()
-        ];
+        $data = data_agenda();
         $this->load->view('administrators/templates/header', $data);
         $this->load->view('administrators/templates/topbar');
         $this->load->view('administrators/templates/sidebar');
@@ -119,11 +70,7 @@ class Administrator extends CI_Controller
     }
     public function history()
     {
-        $all_agenda = $this->db->get('agenda_details')->result_array();
-        $data = [
-            'title' => 'Dashboard',
-            'agenda' => $all_agenda,
-        ];
+        $data = history_agenda();
         $this->load->view('administrators/templates/header', $data);
         $this->load->view('administrators/templates/topbar');
         $this->load->view('administrators/templates/sidebar');
@@ -133,38 +80,23 @@ class Administrator extends CI_Controller
     public function approve()
     {
         if ($_POST) {
-            $id = $this->input->post('id');
-            $this->db->where('id', $id)->update('agenda_details', ['is_verified' => 'accepted', 'agenda_validator' => $this->session->userdata('first_name') . ' ' . $this->session->userdata('last_name')]);
+            $this->Administrator_model->approve_agenda();
             $this->session->set_flashdata('message', '<div class="alert alert-success mt-2" role="alert">Agenda berhasil diverifikasi!</div>');
         }
         redirect('administrator/agenda');
     }
     public function reject()
     {
-        $this->form_validation->set_rules('annotation', 'Annotation', 'required');
-        if ($this->form_validation->run() == false && $_POST) {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger mt-2" role="alert">Keterangan harus diisi!</div>');
-            redirect('administrator/agenda');
-        }
+        reject_validation();
         if ($_POST) {
-            $data = [
-                'id' => $this->input->post('id'),
-                'annotation' => $this->input->post('annotation')
-            ];
-            $this->db->where('id', $data['id'])->update('agenda_details', ['is_verified' => 'declined', 'agenda_validator' => $this->session->userdata('first_name') . $this->session->userdata('last_name'), 'agenda_annotation' => $data['annotation']]);
+            $this->Administrator_model->reject_agenda();
             $this->session->set_flashdata('message', '<div class="alert alert-success mt-2" role="alert">Agenda berhasil ditolak!</div>');
         }
         redirect('administrator/agenda');
     }
     public function passwd()
     {
-        $this->form_validation->set_rules('OldPassword', 'Old Password', 'required|trim');
-        $this->form_validation->set_rules('NewPassword', 'New Password', 'required|trim|matches[NewPassword1]');
-        $this->form_validation->set_rules('NewPassword1', 'Confirm New Password', 'required|trim|matches[NewPassword]');
-        if ($this->form_validation->run() == false && $_POST) {
-            $this->session->set_flashdata('message', '<div class="alert alert-danger mt-2" role="alert">' . validation_errors() . '</div>');
-            redirect('administrator');
-        }
+        changepasswordadmin_validation();
         if ($_POST) {
             $user = $this->db->get_where('user_details', ['id' => $this->session->userdata('id')])->row_array();
             $old_password = $this->input->post('OldPassword');
@@ -182,8 +114,7 @@ class Administrator extends CI_Controller
     public function reset()
     {
         if ($_POST) {
-            $id = $this->input->post('id');
-            $this->db->where('id', $id)->update('user_details', ['password' => password_hash('12345678', PASSWORD_DEFAULT)]);
+            $this->Administrator_model->reset_password();
             $this->session->set_flashdata('message', '<div class="alert alert-success mt-2" role="alert">Password berhasil direset!</div>');
         }
         redirect('administrator/employees');
--- application/models/Employees_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-class Employees_model extends CI_Model
-{
-    public function user()
-    {
-    }
-}
